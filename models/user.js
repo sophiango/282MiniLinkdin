a72c150dbe2564aa69bafbe8435af1cc895a45cd
komaldedhia <komalydedhia@gmail.com>
2015-05-02 22:39:37 -0700

    User Profile UI change

--- models/user.js
+++ models/user.js
@@ -1,43 +1,15 @@
 var mongoose = require('mongoose');
 
-var educationSchema = new mongoose.Schema({
-	id:Number,
-    institution: String,
-    degree: String,
-    fromYear: String,
-    toYear: String
-},{ _id : false });
-
-var experienceSchema = new mongoose.Schema({
-	id:Number,
-    position: String,
-    company: String,
-    from: String,
-    to: String,
-    description:String
-},{ _id : false });
-
-var statusSchema = new mongoose.Schema({
-    content: String,
-    createAt: {type: Date, default: Date.now}
-})
 
 var userSchema = new mongoose.Schema({
     userId : {type : String , required : true, unique: true, dropDups: true},
     firstName: {type : String , required : true},
     lastName: {type : String , required : true},
     imageUrl : String,
-    headline : String,
+    education:String,
     skill1 : String,
     skill2 : String,
     skill3 : String,
-    status: [statusSchema],
-    education:[educationSchema],
-    companyId: String,
-    headlineId: Number,
-    skillOneId : Number,
-    skillTwoId : Number,
-    skillThreeId : Number,
     position1 : String,
     position2 : String,
     position3 : String,
@@ -46,7 +18,21 @@ var userSchema = new mongoose.Schema({
     pid2:Number,
     pid3:Number,
     pid4:Number,
-    positionCount: Number
+    positionCount: Number,
+    company1:String,
+    company2:String,
+    company3:String,
+    company4:String,
+    cId1:Number,
+    cId2:Number,
+    cId3:Number,
+    cId4:Number,
+    headline : String,
+    headlineId: Number,
+    locationId:Number,
+    location:String,
+    createdAt:String,
+    lastUpdated:String
 
 });
 
